b69a46d4cb15e0de2124a4f68d20afd3faa54d8c
Jeffrey Lambach <maincontabaha@gmail.com>
2025-11-21 08:08:10 -0300

    Definir CadastroVIEW como tela principal

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,33 +1,33 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
-    
-    public Connection connectDB(){
+
+    private static final String URL = 
+        "jdbc:mysql://127.0.0.1:3306/leiloes" +
+        "?useSSL=false" +
+        "&allowPublicKeyRetrieval=true" +
+        "&serverTimezone=UTC";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "Q1w@e3r4t5";
+
+    public Connection connectDB() {
         Connection conn = null;
-        
+
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexão realizada com sucesso!");
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar ao banco de dados!");
+            e.printStackTrace();
         }
+
         return conn;
     }
-    
+
+    public static void main(String[] args) {
+        new conectaDAO().connectDB();
+    }
 }
